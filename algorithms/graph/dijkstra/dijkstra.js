e1014ab7b104e58b13711e6334992a421c47a7ba
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-08-14 09:07:58 -0500

    simple algorithm to find the shortest path within a directed or undirected graph

---
+++ algorithms/graph/dijkstra/dijkstra.js
@@ -0,0 +1,8 @@
+import {PriorityQueue} from '../../../data-structures/priority-queue/priority-queue';
+
+export const dijkstra = (graph, start) => {
+    const distances = {};
+    const visited = {};
+    const prev = {};
+    const queue = new PriorityQueue();
+}
