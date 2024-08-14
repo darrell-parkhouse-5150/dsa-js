7fbb9972292f8ddd4566234ab5190d18f63b258e
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-08-14 09:44:52 -0500

    defined the topological sort algorithm, remove the for loop, need to refactor

---
+++ algorithms/uncategorized/topological-sort.ts
@@ -0,0 +1,12 @@
+import { Graph } from "../../data-structures/graph/graph";
+import { Stack } from "../../data-structures/stack/stack";
+
+const topological_sort = (graph: Graph) => {
+    let sorted = new Array();
+    // let count = graph.nodes;
+    let next: Stack = new Stack();
+    
+    for (let i: unknown = 0; i < graph.nodes; i++) {
+
+    }
+}
